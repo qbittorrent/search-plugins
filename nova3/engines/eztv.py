3513283b2973c249818df021aa55da1860a6838a
Alex Duchesne <ducalex007@gmail.com>
2024-08-17 13:56:06 -0400

    Add date field to eztv

--- nova3/engines/eztv.py
+++ nova3/engines/eztv.py
@@ -1,10 +1,12 @@
-#VERSION: 1.15
+#VERSION: 1.16
 # AUTHORS: nindogo
 # CONTRIBUTORS: Diego de las Heras (ngosang@hotmail.es)
 
+import re
 import urllib.error
 import urllib.parse
 import urllib.request
+from datetime import datetime, timedelta
 from html.parser import HTMLParser
 
 from novaprinter import prettyPrinter
@@ -38,6 +40,7 @@ class eztv(object):
                 self.current_item['leech'] = -1
                 self.current_item['size'] = -1
                 self.current_item['engine_url'] = self.url
+                self.current_item['pub_date'] = -1
 
             if (tag == self.A
                     and self.in_table_row and params.get('class') == 'magnet'):
@@ -56,6 +59,23 @@ class eztv(object):
 
             elif self.in_table_row and data.isnumeric():
                 self.current_item['seeds'] = int(data)
+
+            elif self.in_table_row:  # Check for a relative time
+                if m := re.match(r'(\d+)h\s+(\d+)m', data):
+                    date = datetime.now() - timedelta(hours=int(m.group(1)), minutes=int(m.group(2)))
+                    self.current_item['pub_date'] = int(date.timestamp())
+                elif m := re.match(r'(\d+)d\s+(\d+)h', data):
+                    date = datetime.now() - timedelta(days=int(m.group(1)), hours=int(m.group(2)))
+                    self.current_item['pub_date'] = int(date.timestamp())
+                elif m := re.match(r'(\d+)\s+weeks?', data):
+                    date = datetime.now() - timedelta(weeks=int(m.group(1)))
+                    self.current_item['pub_date'] = int(date.timestamp())
+                elif m := re.match(r'(\d+)\s+mo', data):
+                    date = datetime.now() - timedelta(weeks=int(m.group(1)) * 4)
+                    self.current_item['pub_date'] = int(date.timestamp())
+                elif m := re.match(r'(\d+)\s+years?', data):
+                    date = datetime.now() - timedelta(weeks=int(m.group(1)) * 52)
+                    self.current_item['pub_date'] = int(date.timestamp())
 
         def handle_endtag(self, tag):
             if self.in_table_row and tag == self.TR:
