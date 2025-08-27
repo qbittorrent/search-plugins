38c312ecb92ebf52f5e1541de7910ffc2a32241b
Chocobo1 <Chocobo1@users.noreply.github.com>
2025-08-27 18:38:34 +0800

    Move script

--- nova3/aux/fetch_aux.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-cd "$(dirname "$0")" || exit
-git clone --depth 1 https://github.com/qbittorrent/qBittorrent.git
-mv qBittorrent/src/searchengine/nova3/* ./
-rm -rf qBittorrent __init__.py
